9f9dc9e2579459b24d91868e348c650fa1e16de3
LittleBuster <DenisovS21@gmail.com>
2017-04-15 01:33:36 +0700

    remake km

--- kmodule/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-make -C /lib/modules/4.10.8-1-MANJARO/build M=/home/sergey/kmodule modules
--- kmodule/my.c
+++ kmodule/my.c
@@ -1,19 +1,39 @@
+// Netfilter sniffer
+//
+// Copyright (C) 2017 Sergey Denisov.
+// Written by Sergey Denisov aka LittleBuster (DenisovS21@gmail.com)
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public Licence 3
+// as published by the Free Software Foundation; either version 3
+// of the Licence, or (at your option) any later version.
+
 #include <linux/init.h> 
 #include <linux/module.h>
 #include <asm/uaccess.h>
 #include <linux/kernel.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
+#include <linux/skbuff.h>
+#include <linux/udp.h>
+#include <linux/ip.h>
+#include <net/ip.h>
+#include <linux/inet.h>
 
 
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("Sergey Denisov <DenisovS21@gmail.com>"); 
 
-struct nf_hook_ops bundle;
+struct nf_hook_ops nfops;
 
 
 unsigned int HookPackets(uint hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
 {
+	struct iphdr *iph = (struct iphdr *)skb_network_header(skb);
+ 
+	if (htonl(in_aton("192.168.1.1")) == htonl(iph->saddr)) {
+		printk(KERN_INFO "Packet from router was detected!!!\n");
+	}
 	return NF_ACCEPT;
 }
 
@@ -21,19 +41,20 @@ unsigned int HookPackets(uint hooknum, struct sk_buff *skb, const struct net_dev
 static int __init InitModule(void)
 {
         printk(KERN_INFO "Hello, netfilter.\n");
-        bundle.hook = HookPackets;
-        //bundle.owner = THIS_MODULE;
-        bundle.pf = PF_INET;
-        bundle.hooknum = NF_INET_PRE_ROUTING;
-        bundle.priority = NF_IP_PRI_FIRST;
-        nf_register_hook(&bundle);
+
+        nfops.hook = (nf_hookfn *)HookPackets;
+        nfops.pf = PF_INET;
+        nfops.hooknum = 0;
+        nfops.priority = NF_IP_PRI_FIRST;
+
+        nf_register_hook(&nfops);
         return 0;
 }
 
 static void __exit ExitModule(void)
 {
         printk(KERN_INFO "Goodbye, netfilter.\n");
-        nf_unregister_hook(&bundle);
+        nf_unregister_hook(&nfops);
 }
 
 module_init(InitModule);
